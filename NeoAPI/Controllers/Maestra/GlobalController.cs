b753af22c8d474ce32b77ece6bb16252e3588ab8
Gabriel Arcila <gabriel_jesus1122@hotmail.com>
2024-02-28 14:54:27 -0400

    Cambio en NeoAPI.Controllers.Maestras para las consideraciones de la hora y el turno segun el pais

--- NeoAPI/Controllers/Maestra/GlobalController.cs
+++ NeoAPI/Controllers/Maestra/GlobalController.cs
@@ -18,6 +18,7 @@ namespace NeoAPI.Controllers.Maestras
     {
         private readonly DOCIngContext _DOCIng;
         private (int PAVECA, int CHEMPRO, int PANASA, int PAINSA) empresas {get; set;} = (PAVECA: 1,CHEMPRO: 2, PANASA: 3,PAINSA: 4);
+        private (int K10, int K129) centroPAINSA {get; set;} = (K10: 18,K129: 19);
 
         public GlobalController(DOCIngContext DOCIng)
         {
@@ -30,38 +31,136 @@ namespace NeoAPI.Controllers.Maestras
             return TimeZoneInfo.GetSystemTimeZones();
         }
 
-        [HttpGet("GetRotacion/{idPais:int}")]
-        public async Task<ActionResult<RotaCalidum>> GetRotacion(int idPais)
+        [HttpGet("GetConversionHorarios/{idPais:int}")]
+        public ActionResult<DateTime>  GetConversionHorarios(int idPais){
+            DateTime date = DateTime.Now;
+            DateTime dateReal = new DateTime();
+
+            if(idPais == empresas.PAVECA){
+                dateReal = date;
+            }else if(idPais == empresas.CHEMPRO){
+                dateReal = date;
+            }else if(idPais == empresas.PANASA){
+                dateReal = date.AddHours(-1);
+            }else if(idPais == empresas.PAINSA){
+                dateReal = date.AddHours(-2);
+            }else{
+                return BadRequest();
+            }
+            return dateReal;
+        }
+
+        /*
+            //*Consideraciones de Carga de Infomacion:
+                1) Siempre la info se va a guardar en Horario Venezuela
+                2) La definicion del turno actual se va a realizar segun el horario del pais
+                3) La fecha de guardado de los asentamientos va a hacer siempre el dia de comienzo del turno
+        */
+
+        [HttpGet("GetRotacion/{idPais:int}/{idCentro:int}")]
+        public async Task<ActionResult<RotaCalidum>> GetRotacion(int idPais,int idCentro)
         {
-            int hora = DateTime.Now.Hour;
-            int turno = 0; 
+            DateTime dateReal = this.GetConversionHorarios(idPais).Value;
+            int hora = dateReal.Hour;
+            int turno = 0;
             int fecha = 0;
 
 
             if(idPais == this.empresas.PAVECA){
                 if(hora >= 6 && hora < 18){
                     turno = 1;
-                    fecha  = int.Parse(DateTime.Now.ToString("yyyyMMdd"));
+                    fecha = int.Parse(dateReal.ToString("yyyyMMdd"));
                 }else if(hora >= 0 && hora < 6){
                     turno = 2;
-                    fecha = int.Parse(DateTime.Now.AddDays(-1).ToString("yyyyMMdd"));
+                    fecha = int.Parse(dateReal.AddDays(-1).ToString("yyyyMMdd"));
                 }else{
                     turno = 2;
-                    fecha = int.Parse(DateTime.Now.ToString("yyyyMMdd"));
+                    fecha = int.Parse(dateReal.ToString("yyyyMMdd"));
                 }
+
             }else if(idPais == this.empresas.CHEMPRO){
+                if(hora >= 6 && hora < 18){
+                    turno = 1;
+                    fecha = int.Parse(dateReal.ToString("yyyyMMdd"));
+                }else if(hora >= 0 && hora < 6){
+                    turno = 2;
+                    fecha = int.Parse(dateReal.AddDays(-1).ToString("yyyyMMdd"));
+                }else{
+                    turno = 2;
+                    fecha = int.Parse(dateReal.ToString("yyyyMMdd"));
+                }
 
             }else if(idPais == this.empresas.PANASA){
+                if(hora >= 6 && hora < 14){
+                    turno = 1;
+                    fecha  = int.Parse(dateReal.ToString("yyyyMMdd"));
+                }else if(hora >= 14 && hora < 22){
+                    turno = 2;
+                    fecha = int.Parse(dateReal.ToString("yyyyMMdd"));
+                }else{
+                    turno = 3;
+                    if(hora >= 22 && hora < 24){
+                        fecha = int.Parse(dateReal.ToString("yyyyMMdd"));
+                    }else{
+                        fecha = int.Parse(dateReal.AddDays(-1).ToString("yyyyMMdd"));
+                    }
+                }
 
             }else if(idPais == this.empresas.PAINSA){
-            
+                if(idCentro == this.centroPAINSA.K10){
+
+                    if(hora >= 6 && hora < 13){
+                        turno = 1;
+                        fecha = int.Parse(dateReal.ToString("yyyyMMdd"));
+                    }else if(hora >= 13 && hora < 20){
+                        turno = 2;
+                        fecha = int.Parse(dateReal.ToString("yyyyMMdd"));
+                    }else{
+                        turno = 3;
+                        if(hora >= 20 && hora < 24){
+                            fecha = int.Parse(dateReal.ToString("yyyyMMdd"));
+                        }else{
+                            fecha = int.Parse(dateReal.AddDays(-1).ToString("yyyyMMdd"));
+                        }
+                    }
+
+                }else if(idCentro == this.centroPAINSA.K129){
+                    
+                    if(hora >= 7 && hora < 15){
+                        turno = 1;
+                        fecha = int.Parse(dateReal.ToString("yyyyMMdd"));
+                    }else if(hora >= 15 && hora < 23){
+                        turno = 2;
+                        fecha = int.Parse(dateReal.ToString("yyyyMMdd"));
+                    }else{
+                        turno = 3;
+                        if(hora >= 23 && hora < 24){
+                            fecha = int.Parse(dateReal.ToString("yyyyMMdd"));
+                        }else{
+                            fecha = int.Parse(dateReal.AddDays(-1).ToString("yyyyMMdd"));
+                        }
+                    }
+
+                }else{
+                    return BadRequest();
+                }
+
             }else{
                 return BadRequest();
             }
 
             try
             {
-                return await this._DOCIng.RotaCalida.Where(r => r.Rcturno == turno && r.Rcfecha == fecha).FirstOrDefaultAsync() ?? new RotaCalidum();
+                if(idPais == empresas.PAVECA){
+                    return await this._DOCIng.RotaCalida.Where(r => r.Rcturno == turno && r.Rcfecha == fecha).FirstOrDefaultAsync() ?? new RotaCalidum();
+                }else{
+                    RotaCalidum rotaCalidum = new RotaCalidum();
+                    rotaCalidum.RcidRotCal = 0;
+                    rotaCalidum.Rcfecha = fecha;
+                    rotaCalidum.Rcturno = turno;
+                    rotaCalidum.Rcgrupo = "0";
+                    return rotaCalidum;
+                }
             }
             catch
             {
