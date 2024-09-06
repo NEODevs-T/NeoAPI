e9f645f51ba80b57bf7a4c3198c4c23f1c6f6463
geoha <geohaxd@gmail.com>
2024-09-06 11:21:04 -0400

    habilitacion de la propiedad navigation al DTO libronove

--- NeoAPI/DTOs/LibroNovedades/LibroNoveDTO.cs
+++ NeoAPI/DTOs/LibroNovedades/LibroNoveDTO.cs
@@ -40,4 +40,13 @@ public class LibroNoveDTO
 
     public int IdMaster { get; set; }
 
+    public virtual Master? IdMasterNavigation { get; set; } = null;
+
+    public void Deconstruct(out int idLinea, out string idEquipo, out DateTime lnfecha, out bool lnisPizUni)
+    {
+        idLinea = this.IdLinea;
+        idEquipo = this.IdEquipo;
+        lnfecha = this.Lnfecha;
+        lnisPizUni = this.LnisPizUni;
+    }
 }
