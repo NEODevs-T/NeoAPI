ac99188e2d42d8bd03f96658c1a9f190cc042269
geoha <geohaxd@gmail.com>
2024-10-09 09:14:00 -0400

    scalfolding a la base de datos para traer el modelo de EquiposEAM_V Y Añadimiento de su DTO correspondienmte

---
+++ NeoAPI/DTOs/Maestras/EquiposEamVDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NeoAPI.DTOs.Maestra;
+
+public partial class EquiposEamVDTO
+{
+    public int IdEquipo { get; set; }
+
+    public int IdLinea { get; set; }
+
+    public int IdDivision { get; set; }
+
+    public int IdCentro { get; set; }
+
+    public int? IdMaster { get; set; }
+
+    public string Equipo { get; set; } = null!;
+
+    public string Linea { get; set; } = null!;
+
+    public string Centro { get; set; } = null!;
+
+    public string Division { get; set; } = null!;
+
+    public string? Lofic { get; set; }
+}
