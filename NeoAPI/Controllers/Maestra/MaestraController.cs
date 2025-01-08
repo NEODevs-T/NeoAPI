c6f1c582bf8fb3ac8e60e81dc5c2f002eae8573c
Jesus Esquivel <Jesusesquivel513@gmail.com>
2025-01-08 09:44:00 -0400

    Escaneo para CausacalidadV creacion de CausaCalidadVDTO y de su metodo

--- NeoAPI/Controllers/Maestra/MaestraController.cs
+++ NeoAPI/Controllers/Maestra/MaestraController.cs
@@ -696,6 +696,16 @@ namespace NeoAPI.Controllers.Maestras
 
             return Ok(CD);
         }
+
+        [HttpGet("GetPRueba01/{Idcausa:int}")]
+        public async Task<ActionResult<List<CausaCalidadVDTO>>> GetPRueba01(int Idcausa)
+        {
+            List<CausaCalidadV> data = await this._context.CausaCalidadVs.Where(l => l.Idcausa == Idcausa && l.Estado == true).ToListAsync();
+            return Ok(_mapper.Map<List<CausaCalidadVDTO>>(data));
+        }
+
+
+
     }
 
 }
---
+++ NeoAPI/DTOs/Maestras/CausaCalidadVDTO.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace NeoAPI.DTOs.Maestra;
+public partial class CausaCalidadVDTO
+{
+    public int Idregistro { get; set; }
+
+    public string? Centro { get; set; }
+
+    public string? División { get; set; }
+
+    public string? Status { get; set; }
+
+    public DateTime? FechaDeReunion { get; set; }
+
+    public int Idcausa { get; set; }
+
+    public string CausaDeLaCalidad { get; set; } = null!;
+
+    public bool Estado { get; set; }
+}
