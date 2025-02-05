68297abb8f521259a48a51a464c9e6281da51d29
Jesus Esquivel <Jesusesquivel513@gmail.com>
2025-02-05 08:48:03 -0400

    Arreglos de  metodos para filtrar por la asistencia de cada reunion

--- NeoAPI/Controllers/ReunionDiaria/CargoReuController.cs
+++ NeoAPI/Controllers/ReunionDiaria/CargoReuController.cs
@@ -27,7 +27,7 @@ public class CargoReuController: ControllerBase
 
 
         [HttpGet("GetAsistencia/{centro}/{empresa}")]
-        public async Task<ActionResult<List<CargoReuDTO>>> GetAsistencia(string centro, string empresa)
+public async Task<ActionResult<List<CargoReuDTO>>> GetAsistencia(string centro, string empresa)
 {
     try
     {
@@ -35,14 +35,9 @@ public class CargoReuController: ControllerBase
             .Where(a => a.Crarea == centro 
                      && a.Crempresa == empresa 
                      && a.Cresta == true 
-                     && a.IdTipReu == 1)  // Filtramos solo los que tienen IdTipReu = 1
+                     && a.IdTipReu == 1)
             .OrderByDescending(a => a.Crnombre)
             .ToListAsync();
-
-        if (cargoreus == null || !cargoreus.Any())
-        {
-            return NotFound($"No se encontraron registros con centro '{centro}', empresa '{empresa}' y IdTipReu = 3.");
-        }
 
         return Ok(_mapper.Map<List<CargoReuDTO>>(cargoreus));
     }
@@ -52,8 +47,9 @@ public class CargoReuController: ControllerBase
     }
 }
 
-[HttpGet("GetAsistenciadetTurno/{centro}/{empresa}")]
-public async Task<ActionResult<List<CargoReuDTO>>> GetAsistenciadetTurno(string centro, string empresa)    //GetAsistencia
+
+[HttpGet("GetAsistenciadeTurno/{centro}/{empresa}")]
+public async Task<ActionResult<List<CargoReuDTO>>> GetAsistenciadeTurno(string centro, string empresa)
 {
     try
     {
@@ -61,22 +57,18 @@ public async Task<ActionResult<List<CargoReuDTO>>> GetAsistenciadetTurno(string 
             .Where(a => a.Crarea == centro 
                      && a.Crempresa == empresa 
                      && a.Cresta == true 
-                     && a.IdTipReu == 2)  // Filtramos solo los que tienen IdTipReu = 2
+                     && a.IdTipReu == 2)
             .OrderByDescending(a => a.Crnombre)
             .ToListAsync();
 
-        if (cargoreus == null || !cargoreus.Any())
-        {
-            return NotFound($"No se encontraron registros con centro '{centro}', empresa '{empresa}' y IdTipReu = 2.");
-        }
-
-        return Ok(_mapper.Map<List<CargoReuDTO>>(cargoreus));
+        return Ok(_mapper.Map<List<CargoReuDTO>>(cargoreus)); 
     }
     catch (Exception ex)
     {
         return StatusCode(500, $"Error interno del servidor: {ex.Message}");
     }
 }
+
 
 
 [HttpGet("GetAsistenciaQuincenal/{centro}/{empresa}")]
@@ -88,22 +80,18 @@ public async Task<ActionResult<List<CargoReuDTO>>> GetAsistenciaQuincenal(string
             .Where(a => a.Crarea == centro 
                      && a.Crempresa == empresa 
                      && a.Cresta == true 
-                     && a.IdTipReu == 3)  // Filtramos solo los que tienen IdTipReu = 3
+                     && a.IdTipReu == 3)
             .OrderByDescending(a => a.Crnombre)
             .ToListAsync();
 
-        if (cargoreus == null || !cargoreus.Any())
-        {
-            return NotFound($"No se encontraron registros con centro '{centro}', empresa '{empresa}' y IdTipReu = 3.");
-        }
-
-        return Ok(_mapper.Map<List<CargoReuDTO>>(cargoreus));
+        return Ok(_mapper.Map<List<CargoReuDTO>>(cargoreus)); 
     }
     catch (Exception ex)
     {
         return StatusCode(500, $"Error interno del servidor: {ex.Message}");
     }
 }
+
 
 
 }
