91bdec32513ce8ac9bd8592502200a79c43a27a4
Jesus-EET <jesusesquivel513@gmail.com>
2025-12-18 16:02:57 -0400

    cambio de base de datos para el gespline en libro de novedades

--- NeoAPI/Models/Gespline/GesplineContext.cs
+++ NeoAPI/Models/Gespline/GesplineContext.cs
@@ -59,7 +59,7 @@ public partial class GesplineContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=10.20.1.60\\DBVEN01;Initial Catalog=SIPDATABASE;TrustServerCertificate=True;Persist Security Info=True;User ID=portaluser;Password=PORT34erySADF");
+        => optionsBuilder.UseSqlServer("Data Source=10.20.1.60\\DBVEN01;Initial Catalog=SIPDATABASE2;TrustServerCertificate=True;Persist Security Info=True;User ID=portaluser;Password=PORT34erySADF");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
